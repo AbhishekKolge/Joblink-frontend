7e4be5163f65e25e59014f6f9bb00f1af8128766
AbhishekKolge <abhishekkolge96@gmail.com>
2023-01-30 00:36:27 +0530

    credentials removed

--- store/slices/api/apiSlice.js
+++ store/slices/api/apiSlice.js
@@ -6,7 +6,6 @@ import { logoutHandler } from "../../actions/auth/authActions";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: process.env.NEXT_PUBLIC_URL,
-  credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const { accessToken, accessTokenExpirationTime } =
       getState().auth.tokenDetails;
